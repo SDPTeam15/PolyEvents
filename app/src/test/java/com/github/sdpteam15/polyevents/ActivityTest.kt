688281e57bdfb7b302924e193ca4c763d9deb115
Cyrille Pittet <cyrille.pittet@gmail.com>
2021-03-07 15:36:26 +0100

    Added tests for Activity class

--- app/src/test/java/com/github/sdpteam15/polyevents/ActivityTest.kt
+++ app/src/test/java/com/github/sdpteam15/polyevents/ActivityTest.kt
@@ -1,17 +1,44 @@
 package com.github.sdpteam15.polyevents
 
+import com.github.sdpteam15.polyevents.activity.Activity
+import org.hamcrest.CoreMatchers.hasItem
+import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import java.time.LocalDateTime
+import java.util.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Unit tests for the Activity implementation.
  */
+
 class ActivityTest {
+
+    lateinit var activity: Activity
+
+    @Before
+    fun createNewActivity() {
+        activity = Activity("Test Activity","Activity to make tests !",
+                            Pair(LocalDateTime.of(2020, 3, 15, 14, 0),
+                                 LocalDateTime.of(2020, 3, 15, 18, 0)),
+                    "The best organizer", "A", null)
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun addTagIsCorrect() {
+        val newTag = "Food"
+        activity.addTag(newTag)
+        assertThat(activity.tags, hasItem(newTag))
+    }
+
+    @Test
+    fun removeTagIsCorrect() {
+        val newTag = "Movie"
+        activity.addTag(newTag)
+
+        activity.removeTag(newTag)
+        assertThat(activity.tags, not(hasItem(newTag)))
     }
 }
