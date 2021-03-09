35e5c0b379c075a2d9d6a7f1ccf169e9251f4566
Alessio Verardo <alessio.verardo@epfl.ch>
2021-03-09 18:00:56 +0100

    Convert class to object as request in pull request comments

--- app/src/main/java/com/github/sdpteam15/polyevents/helper/HelperFunctions.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/helper/HelperFunctions.kt
@@ -5,19 +5,17 @@ import androidx.fragment.app.FragmentActivity
 import com.github.sdpteam15.polyevents.MainActivity
 import com.github.sdpteam15.polyevents.R
 
-class HelperFunctions private constructor(){
-    companion object{
-        /**
-         * Method that allows to switch the fragment in an activity
-         * @param newFrag: the fragment we want to display (should be in the fragments app from MainActivity otherwise nothing happen)
-         * @param activity: the activity in which a fragment is instantiate
-         */
-        fun changeFragment(activity: FragmentActivity?, newFrag: Fragment?){
-            if(MainActivity.fragments.containsValue(newFrag)){
-                activity?.supportFragmentManager?.beginTransaction()?.apply {
-                    replace(R.id.fl_wrapper,newFrag as Fragment)
-                    commit()
-                }
+object HelperFunctions{
+    /**
+     * Method that allows to switch the fragment in an activity
+     * @param newFrag: the fragment we want to display (should be in the fragments app from MainActivity otherwise nothing happen)
+     * @param activity: the activity in which a fragment is instantiate
+     */
+    fun changeFragment(activity: FragmentActivity?, newFrag: Fragment?){
+        if(MainActivity.fragments.containsValue(newFrag)){
+            activity?.supportFragmentManager?.beginTransaction()?.apply {
+                replace(R.id.fl_wrapper,newFrag as Fragment)
+                commit()
             }
         }
     }
