473fe27bc3ad8174197c6e9a1a4190630cd84fdd
Cyrille Pittet <cyrille.pittet@gmail.com>
2021-03-09 17:43:22 +0100

    Updated gradle dependencies for Mockito

--- app/src/androidTest/java/com/github/sdpteam15/polyevents/UpcomingActivitiesHomeFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdpteam15/polyevents/UpcomingActivitiesHomeFragmentTest.kt
@@ -1,15 +1,12 @@
 package com.github.sdpteam15.polyevents
 
 
-import android.widget.LinearLayout
-import androidx.core.view.children
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.hasChildCount
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.sdpteam15.polyevents.activity.Activity
 import com.github.sdpteam15.polyevents.helper.ActivitiesQueryHelperInterface
 import org.junit.Before
@@ -17,11 +14,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.`when`
-import org.mockito.*
 import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
 import java.time.LocalDateTime
-import androidx.cardview.widget.CardView as CardView
 
 @RunWith(MockitoJUnitRunner::class)
 class UpcomingActivitiesHomeFragmentTest {
