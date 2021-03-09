4c51fd3ac95d78e10549c0ff3e7622b91f93291e
Cyrille Pittet <cyrille.pittet@gmail.com>
2021-03-09 15:58:23 +0100

    Changed incoming activities stubs to helper interface

--- app/src/main/java/com/github/sdpteam15/polyevents/fragments/HomeFragment.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/fragments/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.github.sdpteam15.polyevents.fragments
 
 import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +9,10 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.github.sdpteam15.polyevents.R
 import com.github.sdpteam15.polyevents.activity.Activity
-import java.time.LocalDateTime
+import com.github.sdpteam15.polyevents.helper.ActivitiesQueryHelper
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,15 +33,17 @@ class HomeFragment : Fragment() {
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         val fragmentView = inflater.inflate(R.layout.fragment_home, container, false)
-        val linearLayout = fragmentView.findViewById<LinearLayout>(R.id.upcoming_activities_list)
+        val linearLayout = fragmentView.findViewById<LinearLayout>(R.id.id_upcoming_activities_list)
 
         val activities = getUpcomingActivities()
 
-        for(activity: Activity in activities) {
+        for (activity: Activity in activities) {
             setupActivityTab(activity, linearLayout)
         }
 
@@ -70,33 +72,7 @@ class HomeFragment : Fragment() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun getUpcomingActivities(): List<Activity> {
-        // TODO : Replace these stubs with a query to the database for (sorted) upcoming activities
-        val activities = ArrayList<Activity>()
-
-        activities.add(Activity(
-            "Sushi demo",
-            "Super hungry activity !",
-            LocalDateTime.of(2021, 3, 7, 12, 15),
-            1F,
-            "The fish band",
-            "Kitchen", null))
-
-        activities.add(Activity(
-            "Aqua Poney",
-            "Super cool activity !",
-            LocalDateTime.of(2021, 3, 7, 14, 15),
-            3.5F,
-            "The Aqua Poney team",
-            "Swimming pool", null))
-
-        activities.add(Activity(
-            "Saxophone demo",
-            "Super noisy activity !",
-            LocalDateTime.of(2021, 3, 7, 17, 15),
-            0.75F,
-            "The music band",
-            "Concert Hall", null))
-
-        return activities
+        // TODO : adapt to the db interface
+        return ActivitiesQueryHelper.getUpcomingActivities()
     }
 }
