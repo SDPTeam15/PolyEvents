e3fe4d7cb89d7642fd54e6c490919ff0ea9e372e
Cyrille Pittet <cyrille.pittet@gmail.com>
2021-03-09 15:57:22 +0100

    Added tests for setters/getters for coverage in Activity

--- app/src/test/java/com/github/sdpteam15/polyevents/ActivityTest.kt
+++ app/src/test/java/com/github/sdpteam15/polyevents/ActivityTest.kt
@@ -52,5 +52,49 @@ class ActivityTest {
                 "The best organizer", "A", null)
 
         assertThat(newActivity.durationHours, Is(Activity.DEFAULT_DURATION))
+        newActivity.durationHours = -1.5F
+        assertThat(newActivity.durationHours, Is(Activity.DEFAULT_DURATION))
+    }
+
+    @Test
+    fun setGetNameIsCorrect() {
+        val newName = "New Name de bleu !"
+        activity.name = newName
+        assertThat(activity.name, Is(newName))
+    }
+
+    @Test
+    fun setGetDescriptionIsCorrect() {
+        val newDesc = "New description"
+        activity.description = newDesc
+        assertThat(activity.description, Is(newDesc))
+    }
+
+    @Test
+    fun setGetStartIsCorrect() {
+        val newStart = LocalDateTime.of(2420, 9, 1, 4, 55)
+        activity.start = newStart
+        assertThat(activity.start, Is(newStart))
+    }
+
+    @Test
+    fun setGetDurationIsCorrect() {
+        val newDuration = 3.75F
+        activity.durationHours = newDuration
+        assertThat(activity.durationHours, Is(newDuration))
+    }
+
+    @Test
+    fun setGetOrganizerIsCorrect() {
+        val newOrganizer = "New organizer"
+        activity.organizer = newOrganizer
+        assertThat(activity.organizer, Is(newOrganizer))
+    }
+
+    @Test
+    fun setGetZoneIsCorrect() {
+        val newZone = "New zone"
+        activity.zone = newZone
+        assertThat(activity.zone, Is(newZone))
     }
 }
