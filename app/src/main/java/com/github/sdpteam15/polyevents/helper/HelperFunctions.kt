38643c8f065efd9ad6943af31e1c2931e4039905
Alessio Verardo <alessio.verardo@epfl.ch>
2021-03-09 10:14:15 +0100

    Remove unnecessary functions as we use fragments now

--- app/src/main/java/com/github/sdpteam15/polyevents/helper/HelperFunctions.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/helper/HelperFunctions.kt
@@ -1,30 +1,18 @@
 package com.github.sdpteam15.polyevents.helper
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.text.TextUtils.replace
-import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import com.github.sdpteam15.polyevents.MainActivity
 import com.github.sdpteam15.polyevents.R
 
 class HelperFunctions private constructor(){
     companion object{
-        fun startActivityAndTerminate(context: Context, target: Class<*>){
-            startActivityWithoutTerminate(context,target)
-            (context as? Activity)?.finish()
-        }
-
-        fun startActivityWithoutTerminate(context: Context, target: Class<*>){
-            val loginIntent = Intent(context, target)
-            context.startActivity(loginIntent)
-        }
-
-        fun changeFragment(activity: FragmentActivity?, newFrag: Fragment){
-            activity?.supportFragmentManager?.beginTransaction()?.apply {
-                replace(R.id.fl_wrapper,newFrag)
-                commit()
+        fun changeFragment(activity: FragmentActivity?, newFrag: Fragment?){
+            if(MainActivity.fragments.containsValue(newFrag)){
+                activity?.supportFragmentManager?.beginTransaction()?.apply {
+                    replace(R.id.fl_wrapper,newFrag as Fragment)
+                    commit()
+                }
             }
         }
     }
