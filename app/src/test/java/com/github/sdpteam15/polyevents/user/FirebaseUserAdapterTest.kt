03333fdd4142fe95121b1acccec85341b1490576
Feez <floflodu240399@gmail.com>
2021-03-11 17:15:49 +0100

    removed unused imports

--- app/src/test/java/com/github/sdpteam15/polyevents/user/FirebaseUserAdapterTest.kt
+++ app/src/test/java/com/github/sdpteam15/polyevents/user/FirebaseUserAdapterTest.kt
@@ -1,13 +1,10 @@
 package com.github.sdpteam15.polyevents.user
 
-import com.github.sdpteam15.polyevents.MainActivity
 import com.github.sdpteam15.polyevents.database.FirebaseUserAdapter
-import com.github.sdpteam15.polyevents.database.FirebaseUserInterface
 import com.google.firebase.auth.FirebaseUser
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 import kotlin.test.assertEquals
@@ -19,11 +16,11 @@ private const val uidTest = "Test uid"
 @RunWith(MockitoJUnitRunner::class)
 class FirebaserUserAdapterTest {
     lateinit var mockedFirebaseUser: FirebaseUser
-    lateinit var firebaseUser:FirebaseUserAdapter
+    lateinit var firebaseUser: FirebaseUserAdapter
 
 
     @Before
-    fun setup(){
+    fun setup() {
         mockedFirebaseUser = Mockito.mock(FirebaseUser::class.java)
         firebaseUser = FirebaseUserAdapter(mockedFirebaseUser)
         Mockito.`when`(mockedFirebaseUser.email).thenReturn(emailTest)
@@ -32,10 +29,10 @@ class FirebaserUserAdapterTest {
     }
 
     @Test
-    fun firebaseAdapterReturnCorrectValues(){
-        assertEquals(emailTest,firebaseUser.email)
-        assertEquals(uidTest,firebaseUser.uid)
-        assertEquals(diplayNameTest,firebaseUser.displayName)
+    fun firebaseAdapterReturnCorrectValues() {
+        assertEquals(emailTest, firebaseUser.email)
+        assertEquals(uidTest, firebaseUser.uid)
+        assertEquals(diplayNameTest, firebaseUser.displayName)
     }
 
 }
