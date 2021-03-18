3de81e04bcf78ddaad756989533cb26606c29944
Stephane Selim <stephanesameh_94@outlook.com>
2021-03-18 18:46:25 +0100

    implement Zone entity and zone adapter

--- app/src/main/java/com/github/sdpteam15/polyevents/model/Zone.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/model/Zone.kt
@@ -2,12 +2,15 @@ package com.github.sdpteam15.polyevents.model
 
 import com.google.firebase.firestore.IgnoreExtraProperties
 
+/**
+ * Entity model for a zone. Events occur inside a zone.
+ *
+ * @property zoneName the name of the zone
+ * @property location the location of the zone
+ *
+ */
 @IgnoreExtraProperties
 data class Zone (
-    val zoneName: String,
+    val zoneName: String? = null,
     val location: String? = null
-) {
-    private constructor() : this (
-        "", null
-            )
-}
+)
