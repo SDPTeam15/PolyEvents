fedf8f31ef382caa1b3c47ef0a337845ee869023
Alessio Verardo <alessio.verardo@epfl.ch>
2021-03-20 13:24:24 +0100

    Add the 4 activities that we can access from admin hub

---
+++ app/src/main/java/com/github/sdpteam15/polyevents/admin/UserManagementActivity.kt
@@ -0,0 +1,12 @@
+package com.github.sdpteam15.polyevents.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.sdpteam15.polyevents.R
+
+class UserManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_management)
+    }
+}
