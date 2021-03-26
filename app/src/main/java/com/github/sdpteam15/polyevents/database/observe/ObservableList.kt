1b351903378cc31028d9e708f138133ccbe5e775
Robin Luca Claudio Plumey <robin.plumey@epfl.ch>
2021-03-26 02:28:40 +0100

    ObservableList implement MutableList

--- app/src/main/java/com/github/sdpteam15/polyevents/database/observe/ObservableList.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/database/observe/ObservableList.kt
@@ -3,11 +3,10 @@ package com.github.sdpteam15.polyevents.database.observe
 import androidx.lifecycle.LifecycleOwner
 import com.github.sdpteam15.polyevents.helper.HelperFunctions.observeOnStop
 import com.github.sdpteam15.polyevents.helper.HelperFunctions.run
-import java.util.*
 
-class IndexedValue<T> (val value :T, val index: Int)
+class IndexedValue<T>(val value: T, val index: Int)
 
-class ObservableList<T> : MutableIterable<T>{
+class ObservableList<T> : MutableList<T> {
     private val observersAdd = mutableSetOf<(UpdateArgs<T>) -> Unit>()
     private val observersRemove = mutableSetOf<(UpdateArgs<T>) -> Unit>()
     private val observersItemUpdate = mutableSetOf<(UpdateArgs<IndexedValue<T>>) -> Unit>()
@@ -16,7 +15,7 @@ class ObservableList<T> : MutableIterable<T>{
     private val values: MutableList<Observable<T>> = mutableListOf()
     private val removeItemObserver: MutableMap<Observable<T>, () -> Boolean> = mutableMapOf()
 
-    val size get() = values.size
+    override val size get() = values.size
 
     /**
      * To list of T
@@ -33,7 +32,7 @@ class ObservableList<T> : MutableIterable<T>{
      * Returns the element at the specified index in the list.
      * @return the element at the specified index in the list.
      */
-    operator fun get(index: Int): T = values[index].value!!
+    override operator fun get(index: Int): T = values[index].value!!
 
     /**
      * Returns the observable element at the specified index in the list.
@@ -47,9 +46,15 @@ class ObservableList<T> : MutableIterable<T>{
      * @param value new value
      * @param sender The source of the event.
      */
-    fun set(index: Int, value: T, sender : Any?) = values[index].postValue(value, sender)
+    fun set(index: Int, value: T, sender: Any?): T {
+        val res = this[index]
+        values[index].postValue(value, sender)
+        return res
+    }
 
-    operator fun set(index: Int, value: T) = set(index, value, null)
+    override operator fun set(index: Int, element: T): T = set(index, element, null)
+
+    override fun add(element: T): Boolean = add(element, null) != null
 
     /**
      * Add an observable.
@@ -57,12 +62,28 @@ class ObservableList<T> : MutableIterable<T>{
      * @param sender The source of the event.
      * @return Observable added.
      */
-    fun add(observable: Observable<T>, sender : Any? = null): Observable<T>? = add(observable, sender, true)
-    private fun add(observable: Observable<T>, sender : Any? = null, notify : Boolean): Observable<T>? {
+    fun add(observable: Observable<T>, sender: Any? = null): Observable<T>? =
+        add(observable, sender, true)
+
+    private fun add(
+        observable: Observable<T>,
+        sender: Any? = null,
+        notify: Boolean
+    ): Observable<T>? {
         if (observable.value != null) {
-            values.add(observable)
+            if (!values.add(observable))
+                return null
             removeItemObserver[observable] =
-                observable.observe { itemUpdated(UpdateArgs(IndexedValue(it.value, values.indexOf(observable)), it.sender)) }
+                observable.observe {
+                    itemUpdated(
+                        UpdateArgs(
+                            IndexedValue(
+                                it.value,
+                                values.indexOf(observable)
+                            ), it.sender
+                        )
+                    )
+                }
             itemAdded(UpdateArgs(observable.value!!, sender))
             notifyUpdate(sender)
             return observable
@@ -76,20 +97,26 @@ class ObservableList<T> : MutableIterable<T>{
      * @param sender The source of the event.
      * @return Observable added.
      */
-    fun add(item: T, sender : Any? = null): Observable<T> = add(Observable(item), sender)!!
-    private fun add(item: T, sender : Any? = null, notify : Boolean): Observable<T> = add(Observable(item), sender, notify)!!
+    fun add(item: T, sender: Any? = null): Observable<T>? = add(Observable(item), sender, true)!!
+    private fun add(item: T, sender: Any? = null, notify: Boolean): Observable<T>? =
+        add(Observable(item), sender, notify)!!
+
+    override fun addAll(elements: Collection<T>): Boolean = addAll(elements, null)
 
     /**
      * Add all items in the list.
      * @param items items list.
      * @param sender The source of the event.
      */
-    fun addAll(items: List<T>, sender : Any? = null) {
+    fun addAll(items: Collection<T>, sender: Any? = null): Boolean {
+        var res = true
         for (item: T in items)
-            add(item, sender, false)
+            res = res && (add(item, sender, false) != null)
         notifyUpdate(sender)
+        return res
     }
 
+    override fun remove(element: T): Boolean = remove(element, null) != null
 
     /**
      * Remove an item.
@@ -97,15 +124,20 @@ class ObservableList<T> : MutableIterable<T>{
      * @param sender The source of the event.
      * @return observable removed.
      */
-    fun remove(observable: Observable<T>, sender : Any? = null): Observable<T>? =
+    fun remove(observable: Observable<T>, sender: Any? = null): Observable<T>? =
         remove(observable, sender, true)
-    private fun remove(observable: Observable<T>, sender : Any? = null, notify : Boolean): Observable<T>? {
+
+    private fun remove(
+        observable: Observable<T>,
+        sender: Any? = null,
+        notify: Boolean
+    ): Observable<T>? {
         if (!values.remove(observable))
             return null
         removeItemObserver[observable]!!()
         removeItemObserver.remove(observable)
         itemRemoved(UpdateArgs(observable.value!!, sender))
-        if(notify)
+        if (notify)
             notifyUpdate(sender)
         return observable
     }
@@ -116,21 +148,79 @@ class ObservableList<T> : MutableIterable<T>{
      * @param sender The source of the event.
      * @return observable removed.
      */
-    fun remove(item: T, sender : Any? = null): Observable<T>? {
-        val observable: Observable<T>? = values.find { it.value == item }
+    fun remove(item: T, sender: Any? = null): Observable<T>? = remove(item, sender, true)
+    private fun remove(item: T, sender: Any? = null, notify: Boolean): Observable<T>? {
+        val observable: Observable<T>? = find(item)
         if (observable != null)
-            return remove(observable, sender)
+            return remove(observable, sender, notify)
         return null
     }
+
+    override fun removeAll(elements: Collection<T>): Boolean = removeAll(elements, null)
+
+    /**
+     * Add all items in the list.
+     * @param items items list.
+     * @param sender The source of the event.
+     */
+    fun removeAll(items: Collection<T>, sender: Any? = null): Boolean {
+        var res = true
+        for (item: T in items)
+            res = res && (remove(item, sender, false) != null)
+        notifyUpdate(sender)
+        return res
+    }
+
+    override fun removeAt(index: Int): T = values.removeAt(index).value!!
+
+    override fun clear() = clear(null)
 
     /**
      * Clear all items.
      * @param sender The source of the event.
      */
-    fun clear(sender : Any? = null) {
-        for (item in values)
+    fun clear(sender: Any? = null) {
+        for (item in values.toList())
             remove(item, sender, false)
         notifyUpdate(sender)
+    }
+
+    private fun find(item: T): Observable<T>? = values.find { it.value == item }
+
+    override fun isEmpty(): Boolean = values.isEmpty()
+    override fun contains(element: T): Boolean = (find(element) != null)
+
+    override fun indexOf(element: T): Int {
+        val item = find(element) ?: return -1
+        return values.indexOf(item)
+    }
+
+    override fun containsAll(elements: Collection<T>): Boolean {
+        for (element in elements)
+            if (!this.contains(element))
+                return false
+        return true
+    }
+
+    override fun lastIndexOf(element: T): Int {
+        val item = find(element) ?: return -1
+        return values.lastIndexOf(item)
+    }
+
+    override fun add(index: Int, element: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addAll(index: Int, elements: Collection<T>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun retainAll(elements: Collection<T>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun subList(fromIndex: Int, toIndex: Int): MutableList<T> {
+        TODO("Not yet implemented")
     }
 
     /**
@@ -184,7 +274,10 @@ class ObservableList<T> : MutableIterable<T>{
      *  @param observer lifecycle of the observer to automatically remove it from the observers when stopped
      *  @return a method to remove the observer
      */
-    fun observeUpdate(lifecycle: LifecycleOwner, observer: (UpdateArgs<IndexedValue<T>>) -> Unit): () -> Boolean =
+    fun observeUpdate(
+        lifecycle: LifecycleOwner,
+        observer: (UpdateArgs<IndexedValue<T>>) -> Unit
+    ): () -> Boolean =
         observeOnStop(lifecycle, observeUpdate(observer))
 
     /**
@@ -235,14 +328,25 @@ class ObservableList<T> : MutableIterable<T>{
         }
     }
 
-    inner class ObservableListIterator(var index : Int) : MutableIterator<T>{
+    inner class ObservableListIterator(var index: Int) : MutableListIterator<T> {
         override fun hasNext(): Boolean = values.size > index
         override fun next(): T = get(index++)
+        override fun nextIndex(): Int = index + 1
+
+        override fun hasPrevious(): Boolean = index > 0
+        override fun previous(): T = get(--index)
+        override fun previousIndex(): Int = index
+
+
+        override fun add(element: T) = add(index++, element)
+
         override fun remove() {
-            if(index > 0)
+            if (hasPrevious())
                 remove(getObservable(--index), this)
         }
-
+        override fun set(element: T) {  set(index, element) }
     }
     override fun iterator(): MutableIterator<T> = ObservableListIterator(0)
+    override fun listIterator(): MutableListIterator<T> = ObservableListIterator(0)
+    override fun listIterator(index: Int): MutableListIterator<T> = ObservableListIterator(index)
 }
