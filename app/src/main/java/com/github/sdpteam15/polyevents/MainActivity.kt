d1daf1c7e41485aa85ed2db60f5f29981b664be4
Robin Luca Claudio Plumey <robin.plumey@epfl.ch>
2021-03-31 11:33:57 +0200

    reboot cirrus test

--- app/src/main/java/com/github/sdpteam15/polyevents/MainActivity.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/MainActivity.kt
@@ -45,7 +45,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var adapter: ArrayAdapter<*>
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
