d445b0be2ca8c36177b36c886990870bec6012be
Alessio <alessio.verardo@epfl.ch>
2021-04-07 20:46:40 +0200

    End of graphical tests

--- app/src/androidTest/java/com/github/sdpteam15/polyevents/ZoneManagementTest.kt
+++ app/src/androidTest/java/com/github/sdpteam15/polyevents/ZoneManagementTest.kt
@@ -6,8 +6,6 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.github.sdpteam15.polyevents.admin.ZoneManagementActivity
@@ -29,7 +27,7 @@ import org.mockito.Mockito.`when` as When
 class ZoneManagementTest {
     var mainActivity = ActivityScenarioRule(MainActivity::class.java)
     var zoneManagement = ActivityScenarioRule(ZoneManagementActivity::class.java)
-    lateinit var scenario : ActivityScenario<MainActivity>
+    lateinit var scenario: ActivityScenario<MainActivity>
     lateinit var scenario2: ActivityScenario<ZoneManagementActivity>
 
 
@@ -47,7 +45,7 @@ class ZoneManagementTest {
     @Before
     fun setup() {
         mockedDatabase = Mockito.mock(DatabaseInterface::class.java)
-        val mockedUserProfile = UserProfile("TestID","TestName")
+        val mockedUserProfile = UserProfile("TestID", "TestName")
         When(mockedDatabase.currentProfile).thenReturn(mockedUserProfile)
 
         testUser = UserEntity(
@@ -69,12 +67,15 @@ class ZoneManagementTest {
         Espresso.onView(ViewMatchers.withId(R.id.ic_home)).perform(ViewActions.click())
         Espresso.onView(ViewMatchers.withId(R.id.id_fragment_admin_hub))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.onView(ViewMatchers.withId(R.id.btnRedirectZoneManagement)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.btnRedirectZoneManagement))
+            .perform(ViewActions.click())
 
         Espresso.onView(ViewMatchers.withId(R.id.btnNewZone)).perform(ViewActions.click())
 
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription)).perform(ViewActions.replaceText(""))
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName)).perform(ViewActions.replaceText(""))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription))
+            .perform(ViewActions.replaceText(""))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName))
+            .perform(ViewActions.replaceText(""))
     }
 
     @After
@@ -85,7 +86,7 @@ class ZoneManagementTest {
     }
 
     @Test
-    fun pressCreateButtonDoesNothingWhenEmptyFields(){
+    fun pressCreateButtonDoesNothingWhenEmptyFields() {
 
         Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription))
             .check(ViewAssertions.matches(ViewMatchers.withText("")))
@@ -95,129 +96,177 @@ class ZoneManagementTest {
         Espresso.onView(ViewMatchers.withId(R.id.zone_management_activity)).check(
             ViewAssertions.matches(
                 ViewMatchers.isDisplayed()
-            ))
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName)).perform(ViewActions.replaceText(zoneName))
+            )
+        )
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName))
+            .perform(ViewActions.replaceText(zoneName))
 
         Espresso.onView(ViewMatchers.withId(R.id.btnManage)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.zone_management_activity)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.zone_management_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
-    fun createWithCorrectInfoRedirectToCorrectActivity(){
+    fun createWithCorrectInfoRedirectToCorrectActivity() {
         val obs = Observable<Boolean>()
         When(mockedDatabase.createZone(ZoneManagementActivity.zone)).thenAnswer { _ ->
             obs
         }
 
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName)).perform(ViewActions.replaceText(zoneName))
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription)).perform(ViewActions.replaceText(zoneDesc))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName))
+            .perform(ViewActions.replaceText(zoneName))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription))
+            .perform(ViewActions.replaceText(zoneDesc))
         Espresso.onView(ViewMatchers.withId(R.id.btnManage)).perform(ViewActions.click())
         obs.postValue(true)
 
-        Espresso.onView(ViewMatchers.withId(R.id.zone_management_list_activity)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.zone_management_list_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
-    fun updateInfoRedirectToCorrectActivityIfCorrect(){
-        val zoneInfo = Zone(zoneId, zoneName, zoneLoc,zoneDesc)
+    fun updateInfoRedirectToCorrectActivityIfCorrect() {
+        val zoneInfo = Zone(zoneId, zoneName, zoneLoc, zoneDesc)
 
         val obs = Observable<Boolean>()
         val obs2 = Observable<Boolean>()
-        When(mockedDatabase.getZoneInformation(ZoneManagementActivity.zoneId, ZoneManagementActivity.zoneObservable)).thenAnswer{
+        When(
+            mockedDatabase.getZoneInformation(
+                ZoneManagementActivity.zoneId,
+                ZoneManagementActivity.zoneObservable
+            )
+        ).thenAnswer {
             ZoneManagementActivity.zoneObservable.postValue(zoneInfo)
             obs2
         }
 
-        When(mockedDatabase.updateZoneInformation(ZoneManagementActivity.zoneId, ZoneManagementActivity.zone)).thenAnswer { _ ->
-            obs
-        }
 
-        val intent = Intent(ApplicationProvider.getApplicationContext(), ZoneManagementActivity::class.java)
+        val intent =
+            Intent(ApplicationProvider.getApplicationContext(), ZoneManagementActivity::class.java)
         intent.putExtra(ZoneManagementListActivity.EXTRA_ID, zoneId)
         scenario2 = ActivityScenario.launch(intent)
 
         obs2.postValue(true)
-
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName)).perform(ViewActions.replaceText(zoneName))
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription)).perform(ViewActions.replaceText(zoneDesc+" 2"))
+        When(
+            mockedDatabase.updateZoneInformation(
+                ZoneManagementActivity.zoneId,
+                ZoneManagementActivity.zone
+            )
+        ).thenReturn(obs)
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName))
+            .perform(ViewActions.replaceText(zoneName))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription))
+            .perform(ViewActions.replaceText(zoneDesc + " 2"))
         Espresso.onView(ViewMatchers.withId(R.id.btnManage)).perform(ViewActions.click())
         obs.postValue(true)
 
-        Espresso.onView(ViewMatchers.withId(R.id.zone_management_list_activity)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.zone_management_list_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         scenario2.close()
     }
 
     @Test
-    fun updateWithEmptyFieldsDoesNothing(){
-        val zoneInfo = Zone(zoneId, zoneName, zoneLoc,zoneDesc)
+    fun updateWithEmptyFieldsDoesNothing() {
+        val zoneInfo = Zone(zoneId, zoneName, zoneLoc, zoneDesc)
 
         val obs = Observable<Boolean>()
         val obs2 = Observable<Boolean>()
-        When(mockedDatabase.getZoneInformation(ZoneManagementActivity.zoneId, ZoneManagementActivity.zoneObservable)).thenAnswer{
+        When(
+            mockedDatabase.getZoneInformation(
+                ZoneManagementActivity.zoneId,
+                ZoneManagementActivity.zoneObservable
+            )
+        ).thenAnswer {
             ZoneManagementActivity.zoneObservable.postValue(zoneInfo)
             obs2
         }
 
-        val intent = Intent(ApplicationProvider.getApplicationContext(), ZoneManagementActivity::class.java)
-        intent.putExtra(ZoneManagementListActivity.EXTRA_ID,zoneId)
+        val intent =
+            Intent(ApplicationProvider.getApplicationContext(), ZoneManagementActivity::class.java)
+        intent.putExtra(ZoneManagementListActivity.EXTRA_ID, zoneId)
         scenario2 = ActivityScenario.launch(intent)
 
 
-        When(mockedDatabase.updateZoneInformation(ZoneManagementActivity.zoneId, ZoneManagementActivity.zone)).thenAnswer { _ ->
+        When(
+            mockedDatabase.updateZoneInformation(
+                ZoneManagementActivity.zoneId,
+                ZoneManagementActivity.zone
+            )
+        ).thenAnswer { _ ->
             obs
         }
 
         obs2.postValue(true)
 
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName)).perform(ViewActions.replaceText(zoneName))
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription)).perform(ViewActions.replaceText(""))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName))
+            .perform(ViewActions.replaceText(zoneName))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription))
+            .perform(ViewActions.replaceText(""))
         Espresso.onView(ViewMatchers.withId(R.id.btnManage)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.zone_management_activity)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.zone_management_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
-    fun failToCreateStayOnActivity(){
+    fun failToCreateStayOnActivity() {
         val obs = Observable<Boolean>()
         When(mockedDatabase.createZone(ZoneManagementActivity.zone)).thenAnswer { _ ->
             obs
         }
 
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName)).perform(ViewActions.replaceText("Hello"))
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription)).perform(ViewActions.replaceText("Hello"))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName))
+            .perform(ViewActions.replaceText("Hello"))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription))
+            .perform(ViewActions.replaceText("Hello"))
         Espresso.onView(ViewMatchers.withId(R.id.btnManage)).perform(ViewActions.click())
         obs.postValue(false)
 
-        Espresso.onView(ViewMatchers.withId(R.id.zone_management_activity)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.zone_management_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
-    fun failToUpdateStayOnActivity(){
-        val zoneInfo = Zone(zoneId, zoneName, zoneLoc,zoneDesc)
+    fun failToUpdateStayOnActivity() {
+        val zoneInfo = Zone(zoneId, zoneName, zoneLoc, zoneDesc)
 
         val obs = Observable<Boolean>()
         val obs2 = Observable<Boolean>()
-        When(mockedDatabase.getZoneInformation(ZoneManagementActivity.zoneId, ZoneManagementActivity.zoneObservable)).thenAnswer{
+        When(
+            mockedDatabase.getZoneInformation(
+                ZoneManagementActivity.zoneId,
+                ZoneManagementActivity.zoneObservable
+            )
+        ).thenAnswer {
             ZoneManagementActivity.zoneObservable.postValue(zoneInfo)
             obs2
         }
 
-        val intent = Intent(ApplicationProvider.getApplicationContext(), ZoneManagementActivity::class.java)
-        intent.putExtra(ZoneManagementListActivity.EXTRA_ID,zoneId)
+        val intent =
+            Intent(ApplicationProvider.getApplicationContext(), ZoneManagementActivity::class.java)
+        intent.putExtra(ZoneManagementListActivity.EXTRA_ID, zoneId)
         scenario2 = ActivityScenario.launch(intent)
 
 
-        When(mockedDatabase.updateZoneInformation(ZoneManagementActivity.zoneId, ZoneManagementActivity.zone)).thenAnswer { _ ->
+        When(
+            mockedDatabase.updateZoneInformation(
+                ZoneManagementActivity.zoneId,
+                ZoneManagementActivity.zone
+            )
+        ).thenAnswer { _ ->
             obs
         }
 
         obs2.postValue(true)
 
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName)).perform(ViewActions.replaceText(zoneName))
-        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription)).perform(ViewActions.replaceText(zoneDesc))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementName))
+            .perform(ViewActions.replaceText(zoneName))
+        Espresso.onView(ViewMatchers.withId(R.id.zoneManagementDescription))
+            .perform(ViewActions.replaceText(zoneDesc))
         Espresso.onView(ViewMatchers.withId(R.id.btnManage)).perform(ViewActions.click())
         obs.postValue(true)
 
-        Espresso.onView(ViewMatchers.withId(R.id.zone_management_list_activity)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.zone_management_list_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         scenario2.close()
     }
 
