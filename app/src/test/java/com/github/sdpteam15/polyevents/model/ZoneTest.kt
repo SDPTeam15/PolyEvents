4c069bfd52dfb9114e382d9c7f25570d5912545d
Alessio Verardo <alessio.verardo@epfl.ch>
2021-04-12 21:41:51 +0200

    Added a string to coordinates conversion in ZoneEntity + tests and comments

--- app/src/test/java/com/github/sdpteam15/polyevents/model/ZoneTest.kt
+++ app/src/test/java/com/github/sdpteam15/polyevents/model/ZoneTest.kt
@@ -1,5 +1,7 @@
 package com.github.sdpteam15.polyevents.model
 
+import com.github.sdpteam15.polyevents.database.DatabaseConstant
+import com.google.android.gms.maps.model.LatLng
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -10,10 +12,52 @@ class ZoneTest {
     val zoneName = "zone A"
     val location = "Esplanade"
     val description = "A simple cool zone"
+    val arrayLngLat = arrayOf(4.10, 4.20, 4.30, 4.40, 4.50, 4.60, 4.70, 4.80)
+    val arrayLngLat2 = arrayOf(5.10, 5.20, 5.30, 5.40, 5.50, 5.60, 5.70, 5.80)
+    val allCoords: MutableList<MutableList<LatLng>> = ArrayList()
+    lateinit var list1:MutableList<LatLng>
+    lateinit var list2:MutableList<LatLng>
 
     @Before
     fun setupZone() {
+        list1 = mutableListOf(LatLng(arrayLngLat[0], arrayLngLat[1]),LatLng(arrayLngLat[2], arrayLngLat[3]),LatLng(arrayLngLat[4], arrayLngLat[5]),LatLng(arrayLngLat[6], arrayLngLat[7]))
+        list2 = mutableListOf(LatLng(arrayLngLat2[0], arrayLngLat2[1]),LatLng(arrayLngLat2[2], arrayLngLat2[3]),LatLng(arrayLngLat2[4], arrayLngLat2[5]),LatLng(arrayLngLat2[6], arrayLngLat2[7]))
+        allCoords.add(list1)
+        allCoords.add(list2)
         zone = Zone(zoneId, zoneName, location, description)
+    }
+
+    @Test
+    fun latitudeLongitudeConversionDoneProperly(){
+        var s = ""
+
+        for (c in list1) {
+            s += c.latitude.toString() + DatabaseConstant.LAT_LONG_SEP + c.longitude + DatabaseConstant.POINTS_SEP
+        }
+        s = s.substring(0,s.length-DatabaseConstant.POINTS_SEP.length)+DatabaseConstant.AREAS_SEP
+        for (c in list2) {
+            s += c.latitude.toString() + DatabaseConstant.LAT_LONG_SEP + c.longitude + DatabaseConstant.POINTS_SEP
+        }
+        s = s.substring(0,s.length-DatabaseConstant.POINTS_SEP.length)
+        zone.location = s
+
+        val fromZone = zone.getZoneCoordinates()
+        assert(allCoords.size==fromZone.size)
+        for(i in 0 until allCoords.size){
+            val valid = allCoords[i]
+            val test = fromZone[i]
+            assert(valid.size==test.size)
+            for(j in 0 until valid.size){
+                assert(valid[j]==test[j])
+            }
+        }
+    }
+
+    @Test
+    fun getZoneCoordinatesReturnEmptyListOnNull(){
+        zone.location=null
+        assert(zone.getZoneCoordinates().size ==0)
+        assert(zone.getZoneCoordinates().isEmpty())
     }
 
     @Test
@@ -21,6 +65,6 @@ class ZoneTest {
         assertEquals(zone.zoneName, zoneName)
         assertEquals(zone.location, location)
         assertEquals(zone.zoneId, zoneId)
-        assertEquals(zone.description,description)
+        assertEquals(zone.description, description)
     }
 }
