84cf4a2730251d8eb1db9a1ece4d17a0dfdc65d5
Florian <florian.constantin@epfl.ch>
2021-04-13 19:21:25 +0200

    Added description for Matcher interface

--- app/src/main/java/com/github/sdpteam15/polyevents/database/Matcher.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/database/Matcher.kt
@@ -3,6 +3,10 @@ package com.github.sdpteam15.polyevents.database
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.Query
 
+/**
+ * Filters a firebase collection given some conditions and returns the corresponding Query result.git
+ * For example keep only the first 5 items from the collection.
+ */
 interface Matcher {
     fun match(collection: CollectionReference): Query
 }
