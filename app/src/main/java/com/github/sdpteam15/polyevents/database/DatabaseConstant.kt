e331632f7a50205caf797d92932bd5007593b4ff
Alessio Verardo <alessio.verardo@epfl.ch>
2021-04-15 22:35:23 +0200

    Add enum classes instead of constants

--- app/src/main/java/com/github/sdpteam15/polyevents/database/DatabaseConstant.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/database/DatabaseConstant.kt
@@ -70,4 +70,71 @@ object DatabaseConstant {
      */
     const val LOCATIONS_COLLECTION = "locations"
     const val LOCATIONS_POINT = "point"
+
+    enum class CollectionConstant(val str:String){
+        LOCATION_COLLECTION("locations"),
+        ZONE_COLLECTION("zones"),
+        ITEM_COLLECTION("items"),
+        EVENT_COLLECTION("events"),
+        PROFILE_COLLECTION("Profile"),
+        USER_COLLECTION("users")
+    }
+
+    enum class EventConstant(val str:String){
+        EVENT_COLLECTION("events"),
+        EVENT_DOCUMENT_ID("eventId"),
+        EVENT_NAME("eventName"),
+        EVENT_ZONE_NAME("zoneName"),
+        EVENT_DESCRIPTION("description"),
+        EVENT_ORGANIZER("organizer"),
+        EVENT_START_TIME("startTime"),
+        EVENT_END_TIME("endTime"),
+        EVENT_INVENTORY("inventory"),
+        EVENT_ICON("icon"),
+        EVENT_TAGS("tags")
+    }
+
+    enum class ItemConstants(val str:String){
+        ITEM_COLLECTION("items"),
+        ITEM_DOCUMENT_ID("itemId"),
+        ITEM_NAME("name"),
+        ITEM_TYPE("itemType")
+    }
+
+    enum class ProfileConstants(val str: String){
+        PROFILE_COLLECTION("Profile"),
+        PROFILE_ID("pid"),
+        PROFILE_NAME("name"),
+        PROFILE_RANK("rank")
+    }
+
+    enum class UserConstants(val str: String){
+        USER_UID("uid"),
+        USER_USERNAME("username"),
+        USER_NAME("name"),
+        USER_DISPLAY_NAME("displayName"),
+        USER_EMAIL("email"),
+        USER_AGE("age"),
+        USER_TYPE("userType"),
+        USER_BIRTH_DATE("birthDate"),
+        USER_PHONE("telephone"),
+        USER_PROFILES("profiles")
+    }
+
+    enum class ZoneConstant(val str:String){
+        ZONE_COLLECTION("zones"),
+        ZONE_DOCUMENT_ID("zoneId"),
+        ZONE_NAME("zoneName"),
+        ZONE_LOCATION("zoneLocation"),
+        ZONE_DESCRIPTION("zoneDescription"),
+        LAT_LONG_SEP("|"),
+        POINTS_SEP("!"),
+        AREAS_SEP("?")
+    }
+
+    enum class LocationConstant(val str:String){
+        LOCATIONS_COLLECTION("locations"),
+        LOCATIONS_POINT("point")
+
+    }
 }
