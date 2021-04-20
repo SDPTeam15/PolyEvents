008e88f756a6950e7420b0093a2ab4fab8060296
Alessio Verardo <alessio.verardo@epfl.ch>
2021-04-20 13:55:43 +0200

    Logic behind the activity usermanagementList

--- app/src/main/java/com/github/sdpteam15/polyevents/admin/UserManagementActivity.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/admin/UserManagementActivity.kt
@@ -1,26 +1,23 @@
 package com.github.sdpteam15.polyevents.admin
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.github.sdpteam15.polyevents.EventActivity
 import com.github.sdpteam15.polyevents.R
-import com.github.sdpteam15.polyevents.adapter.EventItemAdapter
-import com.github.sdpteam15.polyevents.database.Database
-import com.github.sdpteam15.polyevents.fragments.EXTRA_EVENT_ID
+import com.github.sdpteam15.polyevents.admin.UserManagementListActivity.Companion.EXTRA_USER_ID
+import com.github.sdpteam15.polyevents.fragments.ProfileFragment
 import com.github.sdpteam15.polyevents.helper.HelperFunctions
-import com.github.sdpteam15.polyevents.model.Event
 
 class UserManagementActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_management)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
+        val intent = intent
+        if (!intent.hasExtra(EXTRA_USER_ID))
+            onBackPressed()
 
-        recyclerView = findViewById(R.id.recycler_view_user)
-        recyclerView.setHasFixedSize(false)
+        val profLog = ProfileFragment(intent.getStringExtra(EXTRA_USER_ID))
+        HelperFunctions.changeFragment(this, profLog, R.id.fl_user_management_profile)
     }
 }
---
+++ app/src/main/java/com/github/sdpteam15/polyevents/admin/UserManagementListActivity.kt
@@ -0,0 +1,59 @@
+package com.github.sdpteam15.polyevents.admin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.github.sdpteam15.polyevents.EventActivity
+import com.github.sdpteam15.polyevents.R
+import com.github.sdpteam15.polyevents.adapter.EventItemAdapter
+import com.github.sdpteam15.polyevents.adapter.UserListAdapter
+import com.github.sdpteam15.polyevents.database.Database
+import com.github.sdpteam15.polyevents.database.Database.currentDatabase
+import com.github.sdpteam15.polyevents.database.observe.ObservableList
+import com.github.sdpteam15.polyevents.fragments.EXTRA_EVENT_ID
+import com.github.sdpteam15.polyevents.helper.HelperFunctions
+import com.github.sdpteam15.polyevents.model.Event
+import com.github.sdpteam15.polyevents.model.UserEntity
+
+class UserManagementListActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private val users = ObservableList<UserEntity>()
+    companion object{
+        val EXTRA_USER_ID = "USER_ID"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_management_list)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
+        recyclerView = findViewById(R.id.recycler_view_user)
+        recyclerView.setHasFixedSize(false)
+
+        val openUser = {
+            user:UserEntity ->
+            val intent = Intent(this, UserManagementActivity::class.java).apply {
+                putExtra(EXTRA_EVENT_ID, user.uid)
+            }
+            startActivity(intent)
+        }
+        recyclerView.adapter = UserListAdapter(users, openUser)
+        getListUsers()
+
+    }
+
+    private fun getListUsers(){
+        currentDatabase.userDatabase!!.getListAllUsers(users).observe {
+            if(it.value){
+               recyclerView.adapter!!.notifyDataSetChanged()
+            }else {
+                HelperFunctions.showToast("Failed to get events information", this)
+            }
+        }
+        /*
+        users.observe(this){
+            recyclerView.adapter!!.notifyDataSetChanged()
+        }*/
+    }
+}
