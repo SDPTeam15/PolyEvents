665800e79b5f1ade199b4f253b60c547c695e699
Feez2403 <floflodu240399@gmail.com>
2021-04-29 12:49:07 +0200

    Shows the item request sender

--- app/src/main/java/com/github/sdpteam15/polyevents/adapter/ItemRequestAdminAdapter.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/adapter/ItemRequestAdminAdapter.kt
@@ -12,20 +12,33 @@ import com.github.sdpteam15.polyevents.R
 import com.github.sdpteam15.polyevents.database.observe.Observable
 import com.github.sdpteam15.polyevents.database.observe.ObservableList
 import com.github.sdpteam15.polyevents.database.observe.ObservableMap
-import com.github.sdpteam15.polyevents.model.Item
 import com.github.sdpteam15.polyevents.model.MaterialRequest
-import com.google.firebase.firestore.auth.User
 
+/**
+ * Recycler Adapter for the list of items
+ * Shows each item with its available quantity
+ * @param context context of parent view used to inflate new views
+ * @param lifecycleOwner parent to enable observables to stop observing when the lifecycle is closed
+ * @param requests List of all item requests
+ * @param userNames Username map to retrieve usernames given their userid
+ */
 class ItemRequestAdminAdapter(
-    context:Context,
+    context: Context,
     lifecycleOwner: LifecycleOwner,
     private val requests: ObservableList<MaterialRequest>,
-    private val userNames: ObservableMap<String,Observable<String>>,
+    private val userNames: ObservableMap<String, Observable<String>>,
+    private val itemNames: ObservableMap<String, String>,
 ) : RecyclerView.Adapter<ItemRequestAdminAdapter.ItemViewHolder>() {
     val adapterLayout =  LayoutInflater.from(context)
     init {
 
         requests.observe(lifecycleOwner) {
+            notifyDataSetChanged()
+        }
+        itemNames.observe(lifecycleOwner) {
+            notifyDataSetChanged()
+        }
+        userNames.observe (lifecycleOwner){
             notifyDataSetChanged()
         }
     }
@@ -35,6 +48,8 @@ class ItemRequestAdminAdapter(
      * Takes the corresponding event view
      */
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+
 
         private val organizer = view.findViewById<TextView>(R.id.id_request_organiser)
         private val time = view.findViewById<TextView>(R.id.id_request_time)
@@ -46,10 +61,9 @@ class ItemRequestAdminAdapter(
          * Binds the values of each view of an event to the layout of an event
          */
         fun bind(request: MaterialRequest) {
-
             organizer.text = userNames[request.userId]!!.value
             time.text = request.time.toString()
-            itemList.text = request.items.map { it.key + ":" +it.value }.joinToString(separator = "\n") { it }
+            itemList.text = request.items.map { itemNames[it.key] + ":" +it.value }.joinToString(separator = "\n") { it }
         }
     }
 
--- app/src/main/java/com/github/sdpteam15/polyevents/admin/ItemRequestManagementActivity.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/admin/ItemRequestManagementActivity.kt
@@ -9,16 +9,20 @@ import com.github.sdpteam15.polyevents.database.Database
 import com.github.sdpteam15.polyevents.database.observe.Observable
 import com.github.sdpteam15.polyevents.database.observe.ObservableList
 import com.github.sdpteam15.polyevents.database.observe.ObservableMap
+import com.github.sdpteam15.polyevents.model.Item
 import com.github.sdpteam15.polyevents.model.MaterialRequest
 import com.github.sdpteam15.polyevents.model.UserEntity
-import com.github.sdpteam15.polyevents.model.UserProfile
 
+/**
+ * Activity to display item requests to Admins
+ * TODO define what happens when an admin accepts an item request
+ */
 class ItemRequestManagementActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private val requests = ObservableList<MaterialRequest>()
-    private val userNames = ObservableMap<String,Observable<String>>()
-    private val itemNames = ObservableMap<String,Observable<String>>()
+    private val userNames = ObservableMap<String, Observable<String>>()
+    private val itemNames = ObservableMap<String, String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +31,26 @@ class ItemRequestManagementActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.id_recycler_item_requests)
 
         Database.currentDatabase.materialRequestDatabase!!.getMaterialRequestList(requests)
-        requests.group(this) { it.userId }.then.map (this,userNames){
+        requests.group(this) { it.userId }.then.map(this, userNames) {
             val s = Observable<UserEntity>()
             Database.currentDatabase.userDatabase!!.getUserInformation(s)
-            s.map (this){ it1-> it1.name?:"" }.then
+            s.map(this) { it1 -> it1.name ?: "" }.then
         }
 
+        val tempItems = ObservableList<Pair<Item, Int>>()
 
-      //  Database.currentDatabase.userDatabase!!.getProfilesUserList(tempUserList,Database.currentDatabase.currentProfile!!)
-        recyclerView.adapter = ItemRequestAdminAdapter(this,this,requests,userNames)
+        tempItems.group(this) { it.first.itemId!! }.then.map(this, itemNames) {
+            it[0].first.itemName!!
+        }
+
+        //Wait until we have both requests accepted from the database to show the material requests
+        Database.currentDatabase.materialRequestDatabase!!.getMaterialRequestList(requests).observeOnce(this) {
+            Database.currentDatabase.itemDatabase!!.getItemsList(tempItems).observeOnce(this) {
+                //  Database.currentDatabase.userDatabase!!.getProfilesUserList(tempUserList,Database.currentDatabase.currentProfile!!)
+                recyclerView.adapter = ItemRequestAdminAdapter(this, this, requests, userNames, itemNames)
+            }
+        }
+
     }
 
 }
