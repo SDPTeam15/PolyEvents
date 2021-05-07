f59c0b72a7fc6a75d869b96d38bd826a3661ca60
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-05-07 17:21:13 +0200

    Fix cast exception

--- app/src/main/java/com/github/sdpteam15/polyevents/model/database/remote/objects/EventDatabase.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/model/database/remote/objects/EventDatabase.kt
@@ -1,5 +1,6 @@
 package com.github.sdpteam15.polyevents.model.database.remote.objects
 
+import android.util.Log
 import com.github.sdpteam15.polyevents.model.database.remote.DatabaseConstant
 import com.github.sdpteam15.polyevents.model.database.remote.DatabaseConstant.CollectionConstant.EVENT_COLLECTION
 import com.github.sdpteam15.polyevents.model.database.remote.DatabaseConstant.CollectionConstant.RATING_COLLECTION
@@ -89,7 +90,6 @@ class EventDatabase(private val db: DatabaseInterface) : EventDatabaseInterface 
         userAccess: UserProfile?
     ): Observable<Boolean> {
         val end = Observable<Boolean>()
-
         val rating = ObservableList<Rating>()
 
         getRatingsForEvent(eventId, null, rating, userAccess).observeOnce {
@@ -116,10 +116,13 @@ class EventDatabase(private val db: DatabaseInterface) : EventDatabaseInterface 
         val rating = ObservableList<Rating>()
 
         db.getListEntity(rating, null, {
+            Log.d(TAG, "ça passe")
+            Log.d(TAG, "ça passe")
             it.whereEqualTo(DatabaseConstant.RatingConstant.RATING_EVENT_ID.value, eventId)
                 .whereEqualTo(DatabaseConstant.RatingConstant.RATING_USER_ID.value, userId)
                 .limit(1)
         }, RATING_COLLECTION).observeOnce {
+            Log.d(TAG, "On est là")
             if (it.value) {
                 if (rating.size == 0) {
                     end.postValue(false, db)
