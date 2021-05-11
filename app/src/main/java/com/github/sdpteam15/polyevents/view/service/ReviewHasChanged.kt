524f4246e4856c7bcaeb93014e51fe218ea4f5fc
Mathieu Marchand <38702331+Mathematou@users.noreply.github.com>
2021-05-11 14:50:27 +0200

    Update app/src/main/java/com/github/sdpteam15/polyevents/view/service/ReviewHasChanged.kt
    Co-authored-by: Rygone <44498456+Rygone@users.noreply.github.com>

--- app/src/main/java/com/github/sdpteam15/polyevents/view/service/ReviewHasChanged.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/view/service/ReviewHasChanged.kt
@@ -3,7 +3,7 @@ package com.github.sdpteam15.polyevents.view.service
 /**
  * Callback for eventActivity when a comment is posted by the current user of when he modifies his comment
  */
-interface ReviewHasChanged {
+fun interface ReviewHasChanged {
     /**
      * Notify review has changed
      */
