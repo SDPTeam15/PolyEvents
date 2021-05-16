c7fd0edc264e377b94a33caf3a3de4c1e288cd3f
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-05-16 21:33:13 +0200

    Add tests for EventManagementListActivity

--- app/src/main/java/com/github/sdpteam15/polyevents/view/activity/admin/EventManagementListActivity.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/view/activity/admin/EventManagementListActivity.kt
@@ -17,9 +17,9 @@ import com.github.sdpteam15.polyevents.view.adapter.EventListAdapter
 class EventManagementListActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
-    var obsEventsMap = ObservableMap<String, Pair<String, ObservableList<Event>>>()
-    private lateinit var modifyListener:(String)->Unit
-    private lateinit var deleteListener:(String,Event)->Unit
+    private var obsEventsMap = ObservableMap<String, Pair<String, ObservableList<Event>>>()
+    private lateinit var modifyListener: (String) -> Unit
+    private lateinit var deleteListener: (String, Event) -> Unit
 
     companion object {
         const val NEW_EVENT_ID = "-1"
@@ -34,56 +34,61 @@ class EventManagementListActivity : AppCompatActivity() {
         setupListeners()
 
         recyclerView = findViewById(R.id.recycler_events_list_admin)
-        recyclerView.adapter = EventListAdapter(this, this, obsEventsMap, modifyListener, deleteListener)
+        recyclerView.adapter =
+            EventListAdapter(this, this, obsEventsMap, modifyListener, deleteListener)
         recyclerView.setHasFixedSize(false)
 
         val requestObservable = ObservableList<Event>()
         requestObservable.group(this) { it.zoneId }.then.observe {
             obsEventsMap.clear()
-            for (i in it.value.entries)
+            for (i in it.value.entries) {
                 obsEventsMap[i.key!!] = Pair(i.value[0].zoneName!!, i.value)
-        }
-
-        currentDatabase.eventDatabase!!.getEvents(null, null, eventList = requestObservable).observe(this) {
-            if (!it.value) {
-                HelperFunctions.showToast(getString(R.string.fail_retrieve_events), this)
-                finish()
             }
         }
+
+        currentDatabase.eventDatabase!!.getEvents(null, null, eventList = requestObservable)
+            .observe(this) {
+                if (!it.value) {
+                    HelperFunctions.showToast(getString(R.string.fail_retrieve_events), this)
+                    finish()
+                }
+            }
 
         findViewById<Button>(R.id.btnNewEvent).setOnClickListener {
             val intent = Intent(this, EventManagementActivity::class.java)
             intent.putExtra(EVENT_ID_INTENT, NEW_EVENT_ID)
             startActivity(intent)
-            // Go back to previous activity
-            finish()
         }
     }
 
-    private fun setupListeners(){
+    private fun setupListeners() {
         modifyListener = { s: String ->
             val intent = Intent(this, EventManagementActivity::class.java)
             intent.putExtra(EVENT_ID_INTENT, s)
             startActivity(intent)
-            // Go back to previous activity
-            finish()
         }
 
-        deleteListener = { zoneId:String,event: Event->
+        deleteListener = { zoneId: String, event: Event ->
             val builder = AlertDialog.Builder(this)
             builder.setMessage("Are you sure to want to delete " + event.eventName + " (this action is not reversible)")
-                    .setPositiveButton(getString(R.string.event_deletion_yes_button_text)) { _, _ ->
-                        currentDatabase.eventDatabase!!.removeEvent(event.eventId!!).observe(this) {
-                            if (it.value) {
-                                HelperFunctions.showToast(getString(R.string.success_delete_from_database), this)
-                                val list = obsEventsMap[zoneId]!!
-                                list.second.remove(event)
-                                obsEventsMap[zoneId] = list
-                            } else {
-                                HelperFunctions.showToast(getString(R.string.fail_delete_from_database), this)
-                            }
+                .setPositiveButton(getString(R.string.event_deletion_yes_button_text)) { _, _ ->
+                    currentDatabase.eventDatabase!!.removeEvent(event.eventId!!).observe(this) {
+                        if (it.value) {
+                            HelperFunctions.showToast(
+                                getString(R.string.success_delete_from_database),
+                                this
+                            )
+                            val list = obsEventsMap[zoneId]!!
+                            list.second.remove(event)
+                            obsEventsMap[zoneId] = list
+                        } else {
+                            HelperFunctions.showToast(
+                                getString(R.string.fail_delete_from_database),
+                                this
+                            )
                         }
-                    }.setNegativeButton(getString(R.string.event_deletion_no_button_text)) { _, _ -> }
+                    }
+                }.setNegativeButton(getString(R.string.event_deletion_no_button_text)) { _, _ -> }
             builder.show()
             Unit
         }
