f0499334068ea59ce0decfaf37a4912b45fbfd42
Mathematou <mathieu.marchand111@gmail.com>
2021-05-18 11:14:02 +0200

    initial commit

---
+++ app/src/main/java/com/github/sdpteam15/polyevents/view/activity/activityprovider/MyItemRequestsActivity.kt
@@ -0,0 +1,12 @@
+package com.github.sdpteam15.polyevents.view.activity.activityprovider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.sdpteam15.polyevents.R
+
+class MyItemRequestsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_item_requests)
+    }
+}
