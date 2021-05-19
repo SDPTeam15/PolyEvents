8a77360f8b94da3a413f0dc2930d1fa3235a995c
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-05-19 22:42:55 +0200

    Add new activity

---
+++ app/src/main/java/com/github/sdpteam15/polyevents/view/activity/activityprovider/EventManagementActivityProvider.kt
@@ -0,0 +1,15 @@
+package com.github.sdpteam15.polyevents.view.activity.activityprovider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.sdpteam15.polyevents.R
+
+class EventManagementActivityProvider : AppCompatActivity() {
+    companion object {
+        const val INTENT_MANAGER = "MANAGER"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_management_provider)
+    }
+}
