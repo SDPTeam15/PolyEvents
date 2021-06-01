2850d1df3b9fabfcdfa33e5da74f848231e98296
Stephane Selim <stephanesameh_94@outlook.com>
2021-06-01 12:38:15 +0200

    Implement a NotificationsHelper object with helper functions to schedule and cancel notifications + implement different notification types.

--- app/src/main/java/com/github/sdpteam15/polyevents/helper/NotificationsHelper.kt
+++ app/src/main/java/com/github/sdpteam15/polyevents/helper/NotificationsHelper.kt
@@ -1,32 +1,170 @@
 package com.github.sdpteam15.polyevents.helper
 
+import android.app.AlarmManager
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import androidx.core.app.AlarmManagerCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.github.sdpteam15.polyevents.R
-import com.github.sdpteam15.polyevents.view.activity.EventActivity
+import com.github.sdpteam15.polyevents.model.room.NotificationUid
+import com.github.sdpteam15.polyevents.view.PolyEventsApplication
 import com.github.sdpteam15.polyevents.view.fragments.EXTRA_EVENT_ID
+import com.github.sdpteam15.polyevents.view.service.AlarmReceiver
+import kotlinx.coroutines.runBlocking
+import java.time.LocalDateTime
 
+const val EXTRA_NOTIFICATION_TYPE = "com.github.sdpteam15.polyevents.helper.NOTIFICATION_TYPE"
 const val EXTRA_NOTIFICATION_ID = "com.github.sdpteam15.polyevents.helper.NOTIFICATION_ID"
 const val EXTRA_NOTIFICATION_MESSAGE = "com.github.sdpteam15.polyevents.helper.NOTIFICATION_MESSAGE"
 
 /**
- * Extension function to the Notification manager, to create a custom notification
+ * Enum class to identify different types of notifications to handle differently
+ * @property notificationType the string value of the NotificationType
+ */
+enum class NotificationType(private val notificationType: String) {
+    EVENT_NOTIFICATION("EVENT_NOTIFICATION"),
+    OTHER_NOTIFICATION("OTHER_NOTIFICATION");
+
+    override fun toString(): String {
+        return notificationType
+    }
+}
+
+/**
+ * Notifications Helper object that offer helper methods to schedule notifications
+ * with the Alarm manager, as well as Notification manager related methods to send
+ * notifications and/or cancel them
+ */
+object NotificationsHelper {
+    /**
+     * Cancel the notification associated with the id provided as well as the pending intent
+     * scheduling the alarm to fire up the notification.
+     * @param notificationId the id of the notification to cancel
+     * @param applicationContext the context of the application
+     */
+    fun cancelNotification(notificationId: Int, applicationContext: Context) {
+        val app = applicationContext as PolyEventsApplication
+
+        // Get the alarm manager from the system
+        val alarmManager =
+            app.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+        val pendingIntent =
+            PendingIntent.getBroadcast(app, notificationId, Intent(app, AlarmReceiver::class.java),
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        if (pendingIntent != null && alarmManager != null) {
+            // Cancel the pending intent
+            alarmManager.cancel(pendingIntent)
+        }
+
+        val notificationManager = ContextCompat.getSystemService(
+            app,
+            NotificationManager::class.java
+        ) as NotificationManager
+        // Cancel the created intent
+        notificationManager.cancelNotification(notificationId)
+    }
+
+    /**
+     * Set the alarm at the scheduled time using the Alarm manager. The alarm manager will notify
+     * the alarm receiver which is a broadcast receiver from inside and outside the app, and will
+     * trigger a notification.
+     */
+    private fun scheduleNotification(
+        scheduledIntent: Intent,
+        scheduledTime: LocalDateTime,
+        applicationContext: Context
+    ): Int {
+        val app = (applicationContext as PolyEventsApplication)
+
+        val newNotificationId = generateNewNotificationId(applicationContext)
+        scheduledIntent.putExtra(EXTRA_NOTIFICATION_ID, newNotificationId)
+
+        // Get the alarm manager from the system
+        val alarmManager = app.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        // Create a pending intent from the intent to be broadcast
+        val notifyPendingIntent = PendingIntent.getBroadcast(
+            app,
+            newNotificationId,
+            scheduledIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        AlarmManagerCompat.setExactAndAllowWhileIdle(
+            alarmManager,
+            AlarmManager.RTC_WAKEUP,
+            HelperFunctions.Converters.fromLocalDateTime(scheduledTime)!!,
+            notifyPendingIntent
+        )
+        Log.d("NotificationsHelper", "Scheduled notification")
+        return newNotificationId
+    }
+
+    /**
+     * Schedule an event notification. For that we need to create an intent with all the necessary
+     * information, that is the event id and the notification message. Then we call schedule
+     * notification with that intent and the notification message
+     * @param eventId the id of the event for which we are scheduling a notification
+     * @param notificationMessage the body of the message for the notification
+     * @param scheduledTime the time at which the notification is scheduled for
+     * @param applicationContext the context of the application
+     */
+    fun scheduleEventNotification(
+        eventId: String,
+        notificationMessage: String,
+        scheduledTime: LocalDateTime,
+        applicationContext: Context
+    ): Int {
+        val app = (applicationContext as PolyEventsApplication)
+        // Set the alarm before the event start by 15 min
+        val notifyEventIntent = Intent(app, AlarmReceiver::class.java).apply {
+            putExtra(EXTRA_NOTIFICATION_TYPE, NotificationType.EVENT_NOTIFICATION.toString())
+            putExtra(EXTRA_EVENT_ID, eventId)
+            putExtra(EXTRA_NOTIFICATION_MESSAGE, notificationMessage)
+        }
+
+        return scheduleNotification(
+            scheduledIntent = notifyEventIntent,
+            scheduledTime = scheduledTime,
+            applicationContext = applicationContext
+        )
+    }
+
+    /**
+     * Generate a new notification id for a notification
+     */
+    private fun generateNewNotificationId(applicationContext: Context): Int = runBlocking {
+        val app = (applicationContext as PolyEventsApplication)
+        val notificationUid = app.database.notificationUidDao().getNotificationUid()
+        if (notificationUid == null) {
+            // If no notificationUid instance already created, create one
+            app.database.notificationUidDao().insert(NotificationUid(uid = 1))
+            0
+        } else {
+            app.database.notificationUidDao()
+                .insert(notificationUid.copy(uid = notificationUid.uid + 1))
+            notificationUid.uid
+        }
+    }
+}
+
+/**
+ * Extension function to the Notification manager, to create a custom notification.
  * @param messageBody the message that will be displayed in the body of the notification
  * @param applicationContext the context of the application
- * @param eventId the eventId of the event of which the user will be notified
  * @param notificationId the id for the notification to be set
+ * @param contentIntent the intent to be launched, when clicking on the notification
  */
 fun NotificationManager.sendEventNotification(
-    messageBody: String, applicationContext: Context, eventId: String, notificationId: Int
+    messageBody: String,
+    applicationContext: Context,
+    notificationId: Int,
+    contentIntent: Intent?
 ) {
-    // Create the content intent for the notification, which launches
-    // this activity
-    val contentIntent = Intent(applicationContext, EventActivity::class.java).apply {
-        putExtra(EXTRA_EVENT_ID, eventId)
-    }
 
     /**
      * We created the intent, but the notification is displayed outside the app.
@@ -50,7 +188,7 @@ fun NotificationManager.sendEventNotification(
     // Build the notification
     val builder = NotificationCompat.Builder(
         applicationContext,
-        applicationContext.getString(R.string.event_notification_channel_id)
+        applicationContext.getString(R.string.notification_channel_id)
     ).setSmallIcon(R.drawable.ic_event)        // TODO: replace with application icon
         .setContentTitle(applicationContext.getString(R.string.app_name))
         .setContentText(messageBody)
