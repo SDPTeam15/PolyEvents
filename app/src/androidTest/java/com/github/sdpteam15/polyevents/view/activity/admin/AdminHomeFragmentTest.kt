a447f1f07ae357d1ae1f935fc4a7288e5cb5c501
AlessioVerardo <44498464+AlessioVerardo@users.noreply.github.com>
2021-06-03 16:10:33 +0200

    Comments tests that will be merged with another branch

--- app/src/androidTest/java/com/github/sdpteam15/polyevents/view/activity/admin/AdminHomeFragmentTest.kt
+++ app/src/androidTest/java/com/github/sdpteam15/polyevents/view/activity/admin/AdminHomeFragmentTest.kt
@@ -71,77 +71,77 @@ class AdminHubFragmentTest {
         Intents.release()
         Database.currentDatabase = FirestoreDatabaseProvider
     }
-
-    @Test
-    fun clickOnBtnItemRequestManagementDisplayCorrectActivity() {
-        Database.currentDatabase = FakeDatabase
-        Espresso.onView(ViewMatchers.withId(R.id.btnRedirectItemReqManagement)).perform(click())
-        Intents.intended(IntentMatchers.hasComponent(ItemRequestManagementActivity::class.java.name))
-    }
-
-    @Test
-    fun clickOnBtnZoneManagementDisplayCorrectActivity() {
-        Espresso.onView(ViewMatchers.withId(R.id.btnRedirectZoneManagement)).perform(click())
-        Intents.intended(IntentMatchers.hasComponent(ZoneManagementListActivity::class.java.name))
-    }
-
-    @Test
-    fun clickOnBtnEventDisplayCorrectActivity() {
-        val mockedDatabase = HelperTestFunction.defaultMockDatabase()
-        Database.currentDatabase = mockedDatabase
-        val mockedEventDatabase = mock(EventDatabaseInterface::class.java)
-        Mockito.`when`(mockedDatabase.eventDatabase).thenReturn(mockedEventDatabase)
-
-        Mockito.`when`(
-            mockedEventDatabase.getEvents(
-                anyOrNull(),
-                anyOrNull(),
-                anyOrNull()
-            )
-        ).thenReturn(
-            Observable(true)
-        )
-        Espresso.onView(ViewMatchers.withId(R.id.btnRedirectEventManager)).perform(click())
-        Intents.intended(IntentMatchers.hasComponent(EventManagementListActivity::class.java.name))
-    }
-
-    @Test
-    fun clickOnBtnUserManagementDisplayCorrectActivity() {
-        Database.currentDatabase = FakeDatabase
-        Espresso.onView(ViewMatchers.withId(R.id.btnRedirectUserManagement)).perform(click())
-        Intents.intended(IntentMatchers.hasComponent(UserManagementListActivity::class.java.name))
-    }
-
     /*
-    @Test
-    fun itemRequestActivityOpensOnClick() {
-        Espresso.onView(ViewMatchers.withId(R.id.ic_more)).perform(click())
+     @Test
+     fun clickOnBtnItemRequestManagementDisplayCorrectActivity() {
+         Database.currentDatabase = FakeDatabase
+         Espresso.onView(ViewMatchers.withId(R.id.btnRedirectItemReqManagement)).perform(click())
+         Intents.intended(IntentMatchers.hasComponent(ItemRequestManagementActivity::class.java.name))
+     }
 
-        Espresso.onView(ViewMatchers.withId(R.id.btnRedirectItemReqManagement)).perform(click())
-        Intents.intended(IntentMatchers.hasComponent(ItemRequestActivity::class.java.name))
-    }
+     @Test
+     fun clickOnBtnZoneManagementDisplayCorrectActivity() {
+         Espresso.onView(ViewMatchers.withId(R.id.btnRedirectZoneManagement)).perform(click())
+         Intents.intended(IntentMatchers.hasComponent(ZoneManagementListActivity::class.java.name))
+     }
 
-    @Test
-    fun itemsAdminActivity() {
-        var availableItems: MutableMap<Item, Int> = mutableMapOf()
-        availableItems[Item(null, "Chocolat", "OTHER")] = 30
-        availableItems[Item(null, "Kiwis", "OTHER")] = 10
-        availableItems[Item(null, "230V Plugs", "PLUG")] = 30
-        availableItems[Item(null, "Fridge (large)", "OTHER")] = 5
-        availableItems[Item(null, "Cord rewinder (15m)", "PLUG")] = 30
-        availableItems[Item(null, "Cord rewinder (50m)", "PLUG")] = 10
-        availableItems[Item(null, "Cord rewinder (25m)", "PLUG")] = 20
+     @Test
+     fun clickOnBtnEventDisplayCorrectActivity() {
+         val mockedDatabase = HelperTestFunction.defaultMockDatabase()
+         Database.currentDatabase = mockedDatabase
+         val mockedEventDatabase = mock(EventDatabaseInterface::class.java)
+         Mockito.`when`(mockedDatabase.eventDatabase).thenReturn(mockedEventDatabase)
 
-        Database.currentDatabase = FakeDatabase
+         Mockito.`when`(
+             mockedEventDatabase.getEvents(
+                 anyOrNull(),
+                 anyOrNull(),
+                 anyOrNull()
+             )
+         ).thenReturn(
+             Observable(true)
+         )
+         Espresso.onView(ViewMatchers.withId(R.id.btnRedirectEventManager)).perform(click())
+         Intents.intended(IntentMatchers.hasComponent(EventManagementListActivity::class.java.name))
+     }
 
-        FakeDatabaseItem.items.clear()
-        for ((item, count) in availableItems) {
-            Database.currentDatabase.itemDatabase!!.createItem(item, count)
-        }
+     @Test
+     fun clickOnBtnUserManagementDisplayCorrectActivity() {
+         Database.currentDatabase = FakeDatabase
+         Espresso.onView(ViewMatchers.withId(R.id.btnRedirectUserManagement)).perform(click())
+         Intents.intended(IntentMatchers.hasComponent(UserManagementListActivity::class.java.name))
+     }
 
-        Espresso.onView(ViewMatchers.withId(R.id.ic_more)).perform(click())
-        Espresso.onView(ViewMatchers.withId(R.id.btn_admin_items_list)).perform(click())
-        Intents.intended(IntentMatchers.hasComponent(ItemsAdminActivity::class.java.name))
-    }
-    */
+
+     @Test
+     fun itemRequestActivityOpensOnClick() {
+         Espresso.onView(ViewMatchers.withId(R.id.ic_more)).perform(click())
+
+         Espresso.onView(ViewMatchers.withId(R.id.btnRedirectItemReqManagement)).perform(click())
+         Intents.intended(IntentMatchers.hasComponent(ItemRequestActivity::class.java.name))
+     }
+
+     @Test
+     fun itemsAdminActivity() {
+         var availableItems: MutableMap<Item, Int> = mutableMapOf()
+         availableItems[Item(null, "Chocolat", "OTHER")] = 30
+         availableItems[Item(null, "Kiwis", "OTHER")] = 10
+         availableItems[Item(null, "230V Plugs", "PLUG")] = 30
+         availableItems[Item(null, "Fridge (large)", "OTHER")] = 5
+         availableItems[Item(null, "Cord rewinder (15m)", "PLUG")] = 30
+         availableItems[Item(null, "Cord rewinder (50m)", "PLUG")] = 10
+         availableItems[Item(null, "Cord rewinder (25m)", "PLUG")] = 20
+
+         Database.currentDatabase = FakeDatabase
+
+         FakeDatabaseItem.items.clear()
+         for ((item, count) in availableItems) {
+             Database.currentDatabase.itemDatabase!!.createItem(item, count)
+         }
+
+         Espresso.onView(ViewMatchers.withId(R.id.ic_more)).perform(click())
+         Espresso.onView(ViewMatchers.withId(R.id.btn_admin_items_list)).perform(click())
+         Intents.intended(IntentMatchers.hasComponent(ItemsAdminActivity::class.java.name))
+     }
+     */
 }
